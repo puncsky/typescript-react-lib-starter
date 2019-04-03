2023c8e0184e5997656c86a5bf7c90002dbe50d0
Tian Pan <puncsky@gmail.com>
2019-04-03 15:29:56 -0700

    add initial react lib

--- ava.config.js
+++ ava.config.js
@@ -1,12 +1,6 @@
 export default {
-    require: [
-        'ts-node/register',
-    ],
-    files: [
-        'src/**/*.test.ts',
-    ],
-    compileEnhancements: false,
-    extensions: [
-        'ts',
-    ],
+  require: ["ts-node/register"],
+  files: ["src/**/*.test.ts", "src/**/*.test.tsx"],
+  compileEnhancements: false,
+  extensions: ["ts", "tsx"]
 };
--- src/__test__/index.test.ts
+++
@@ -1,7 +0,0 @@
-import test from "ava";
-import { delayedHello } from "..";
-
-test("delayedHello test", async t => {
-  const result = await delayedHello("delayedHello");
-  t.deepEqual(result, "Hello, delayedHello");
-});
